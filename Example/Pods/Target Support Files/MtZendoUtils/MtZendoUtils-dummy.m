7209817cba9cffb0d4686525afaf55b79b043343
Victor Chen <yche276@gmail.com>
2019-05-18 22:10:00 +1200

    1: remove pods folder

--- Example/Pods/Target Support Files/MtZendoUtils/MtZendoUtils-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MtZendoUtils : NSObject
-@end
-@implementation PodsDummy_MtZendoUtils
-@end
--- Example/Pods/Target Support Files/Pods-MtZendoUtils_Tests/Pods-MtZendoUtils_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MtZendoUtils_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_MtZendoUtils_Tests
-@end
